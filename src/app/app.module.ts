12aac2a93ea82adff899c2d68c14fc0ca6d353fc
Yevhenii-Maksimishyn <evgenijmaksimisin@gmail.com>
2022-04-24 18:38:29 +0200

    Create modal comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToggleButtonsComponent } from './components/toggle-buttons/toggle-buttons.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToggleButtonsComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
